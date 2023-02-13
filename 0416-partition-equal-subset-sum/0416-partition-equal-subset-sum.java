51803cc0359b412b61b3355f6f1a24f90d9592a6
Aditya Nautiyal <76645509+adityan150@users.noreply.github.com>
2023-02-13 17:56:55 +0530

    Time: 62 ms (50.60%), Space: 44.8 MB (54.25%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,18 +6,33 @@ class Solution {
         if (sum % 2 != 0) return false;
         sum = sum / 2;
         
-        Boolean[][] dp = new Boolean[nums.length][sum+1];
+        boolean[][] dp = new boolean[nums.length+1][sum+1];
+        // initialization
+        for (int i = 0; i <= nums.length; i++)
+            dp[i][0] = true;
+        for (int i = 1; i <= sum; i++)
+            dp[0][i] = false;
         
-        return subsetSum(nums, sum, 0, dp);
+        // tabulation
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (j >= nums[i-1])
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return dp[nums.length][sum];
     }
     
-    boolean subsetSum(int[] nums, int t, int i, Boolean[][] dp) {
-        if (t == 0) return true;
-        if (i == nums.length || t < 0) return false;
+//     boolean subsetSum(int[] nums, int t, int i, Boolean[][] dp) {
+//         if (t == 0) return true;
+//         if (i == nums.length || t < 0) return false;
         
-        if (dp[i][t] != null) return dp[i][t];
+//         if (dp[i][t] != null) return dp[i][t];
         
-        return dp[i][t] = subsetSum(nums, t - nums[i], i+1, dp)
-            || subsetSum(nums, t, i+1, dp);
-    }
+//         return dp[i][t] = subsetSum(nums, t - nums[i], i+1, dp)
+//             || subsetSum(nums, t, i+1, dp);
+//     }
 }
